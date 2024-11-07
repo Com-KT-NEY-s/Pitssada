10e40351a557302046c48b041284300a407e8a69
Thunder <151688223+Mathiack@users.noreply.github.com>
2024-11-07 19:16:20 -0300

    problema de crash resolvido

--- v0.8.2mt/src/main/java/Inicio/home.java
+++ v0.8.2mt/src/main/java/Inicio/home.java
@@ -65,7 +65,7 @@ public class home extends javax.swing.JFrame {
     public void startDatabaseConnectionChecker() {
         connectionChecker = Executors.newScheduledThreadPool(1);
         connectionChecker.scheduleAtFixedRate(() -> {
-            Thread.currentThread().setName("Timer");
+            System.out.println("bom dia");
             try {
                 checkDatabaseConnection();
             } catch (Exception ex) {
@@ -76,16 +76,16 @@ public class home extends javax.swing.JFrame {
     }
 
     public void checkDatabaseConnection() {
-        home h = new home();
         try {
             connection = getConnection();
-            h.updateConnectionStatusPanel(connection != null && !connection.isClosed());
+            updateConnectionStatusPanel(connection != null && !connection.isClosed());
             if (connection != null) {
                 connection.close();
+                connection = null;
             }
         } catch (SQLException | NullPointerException ex) {
             System.err.println("Erro ao conectar ao banco de dados: " + ex.getMessage());
-            h.updateConnectionStatusPanel(false);
+            updateConnectionStatusPanel(false);
         }
     }
 
